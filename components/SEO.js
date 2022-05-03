94eebecfad18399e90219d178fc865b025aac476
Rachel Kim <yek99kr@gmal.com>
2022-05-03 14:17:53 -0400

    a

--- components/SEO.js
+++ components/SEO.js
@@ -16,12 +16,12 @@ const SEO = () => {
         content="We are a full service creative studio specializing in good ideas. We create original, experimental, and interactive work."
       />
       <meta property="og:url" content="realgood.tv" />
-      {/* <meta property="twitter:title" content="Real Good Studio" />
+      <meta property="twitter:title" content="Real Good Studio" />
       <meta
         property="twitter:description"
         content="We are a full service creative studio specializing in good ideas. We create original, experimental, and interactive work."
       />
-      <meta
+      {/* <meta
         property="twitter:image"
         content="https://realgood.tv/img/og-image.png"
       />
@@ -35,18 +35,18 @@ const SEO = () => {
       />
       <meta property="twitter:image:alt" content="Real Good Studio" />
       <meta property="og:site_name" content="Real Good Studio" /> */}
-      {/* <link
+      <link
         rel="icon"
         type="image/png"
-        href="img/favicon-32x32.png"
+        href="favicon/favicon-32x32.png"
         sizes="32x32"
       />
       <link
         rel="icon"
         type="image/png"
-        href="img/favicon-16x16.png"
+        href="favicon/favicon-16x16.png"
         sizes="16x16"
-      /> */}
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
   );
